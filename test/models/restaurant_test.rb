15fdc3f083512f9b497da30cfd3d2a76ac92a43d
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-09-03 16:58:04 +0200

    Added test for restaurant model

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates name and address presence' do
+    city = places(:lausanne)
+    restaurant = Restaurant.new(name: '', address: '', place_id: city.id)
+    restaurant.validate
+
+    assert restaurant.errors.to_a, "Name can't be blank"
+    assert restaurant.errors.to_a, "Address can't be blank"
+  end
 end
