246bc1aad6a3b0b9bd667443e2b448ccea55dba0
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-10-01 13:27:13 +0200

    Restaurant tests

--- test/controllers/restaurant_controller_test.rb
+++ test/controllers/restaurant_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class RestaurantControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the restaurant list gets shown' do
+    get place_restaurants_path(places(:lausanne))
+    assert_response :success
+  end
+
+  test 'check if restaurant/new gets shown' do
+    get new_place_restaurant_path(places(:lausanne))
+    assert_response :success
+  end
+
+  test 'a restaurant gets created' do
+    lausanne = places(:lausanne)
+    restaurant_params = { restaurant: { name: 'test creation',
+                                        address: 'test creation',
+                                        place_id: lausanne.id } }
+    post place_restaurants_path(lausanne), params: restaurant_params
+    assert_redirected_to place_restaurants_path
+  end
+
+  test 'check if restaurant/edit gets shown' do
+    lausanne = places(:lausanne)
+    restaurant_1 = lausanne.restaurants.first
+    get edit_place_restaurant_path(lausanne, restaurant_1)
+    assert_response :success
+  end
+
+  test 'a restaurant gets updated' do
+    lausanne = places(:lausanne)
+    restaurant_1 = lausanne.restaurants.first
+    patch place_restaurant_path(lausanne, restaurant_1), params: { restaurant: { name: 'new name', address: 'new address' } }
+    assert_redirected_to place_restaurants_path 
+  end
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,24 +1,16 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-<<<<<<< HEAD
   test 'validates name and address presence' do
     city = places(:lausanne)
     restaurant = Restaurant.new(name: '', address: '', place_id: city.id)
     restaurant.validate
-
-    assert restaurant.errors.to_a, "Name can't be blank"
-    assert restaurant.errors.to_a, "Address can't be blank"
+    assert_includes restaurant.errors.to_a, "Name can't be blank" 
+    assert_includes restaurant.errors.to_a, "Address can't be blank"
   end
 
   test 'validates restaurant search' do
     restaurant = Restaurant.find_by(place_id: places(:lausanne).id)
-
-    assert restaurant.name, 'Test restaurant name'
+    assert_equal restaurant.name, 'Test restaurant name' 
   end
-=======
-  # test "the truth" do
-  #   assert true
-  # end
->>>>>>> 1b818e97dbefb7fc51efc46f6ccb8aa624a2cb90
 end
