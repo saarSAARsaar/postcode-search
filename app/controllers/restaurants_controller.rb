5d3ffc4688dad1dc5f0bdcef9bcdb3666699eee7
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-10-01 13:26:44 +0200

    Restaurant CRUD

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,21 +1,58 @@
 class RestaurantsController < ApplicationController
     before_action :get_place
+    before_action :get_restaurant, only: %i[edit update destroy]
 
     def index
         @restaurants = @place.restaurants
     end
 
     def show
-        
+
+    end
+
+    def new
+        @restaurant = @place.restaurants.new()
+    end
+
+    def create
+        @restaurant = @place.restaurants.new(restaurant_params)
+        if @restaurant.save
+          flash[:notice] = 'Restaurant has been created'
+          redirect_to place_restaurants_path(@place)
+        else
+          render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+      if @restaurant.update(restaurant_params)
+        redirect_to place_restaurants_path(@place), notice: 'Restaurant has been edited'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @place = get_place()
+      get_restaurant().destroy
+
+      redirect_to @place
     end
 
     private
 
     def get_place
-        @place = Place.find(params[:place_id])
+      @place = Place.find(params[:place_id])
+    end
+
+    def get_restaurant
+      @restaurant = Restaurant.find(params[:id])
     end
 
     def restaurant_params
-        params.require(:restaurant).permit(:name, :adress, :place_id)
+        params.require(:restaurant).permit(:name, :address, :place_id)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,5 @@
 class Restaurant < ApplicationRecord
-<<<<<<< HEAD
     belongs_to :place
 
-=======
->>>>>>> 1b818e97dbefb7fc51efc46f6ccb8aa624a2cb90
     validates :name, :address, presence: true
 end
