2989cc277be2a38d783883306b30d85b6f1dd46d
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-10-22 14:28:05 +0200

    Changed one test to use a notice and cleaned up other parts

--- test/controllers/place_finder_controller_test.rb
+++ test/controllers/place_finder_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class PlaceFinderControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  
-  
 end
--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -4,21 +4,21 @@ class RestaurantsTest < ApplicationSystemTestCase
   test 'display restaurants on place show page' do
     visit root_path
 
-    within 'body > table > tbody > tr:nth-child(3)' do
+    within 'table > tbody > tr:nth-child(3)' do
       assert_selector 'td', text: 'Lausanne'
       click_on 'Show'
     end
 
     click_on 'Restaurants'
 
-    within all('body > table > tbody > tr').first do 
+    within all('table > tbody > tr').first do 
       assert_selector 'td', text: 'Test restaurant name'
     end
   end
 
   test 'newly created restaurant gets shown in the list' do
     visit root_path
-    within 'body > table > tbody > tr:nth-child(3)' do 
+    within 'table > tbody > tr:nth-child(3)' do 
       assert_selector 'td', text: 'Lausanne'
       click_on 'Show'
     end
@@ -29,8 +29,8 @@ class RestaurantsTest < ApplicationSystemTestCase
     fill_in("Address", with: "test address")
 
     click_on 'Save'
-    assert_selector 'body', text: 'Restaurant has been created'
-    within all('body > table > tbody > tr').last do
+    assert_selector '.notice', text: 'Restaurant has been created'
+    within all('table > tbody > tr').last do
       assert_selector 'td', text: 'test name'
     end
   end
