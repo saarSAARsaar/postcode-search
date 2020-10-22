f176208e4db2384df34677af79b2f658c2d1282b
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-10-22 14:29:16 +0200

    Before_action inclusion and turned get_ to set_

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,36 +1,28 @@
 class PlacesController < ApplicationController
-    def index
-        @search = Search.new
-        @results = @search.results
-        @data_length = @search.data_length
-        @cantons = Place.all.pluck(:canton).uniq.append("All")
+	before_action :set_place, except: %i[index]
+
+  def index
+    @search = Search.new
+    @results = @search.results
+    @data_length = @search.data_length
+    @cantons = Place.all.pluck(:canton).uniq.unshift("All") 
+  end
+
+  def update 
+    if @place.update(places_params)
+      redirect_to @place
+    else
+      render 'edit'
     end
+  end
 
-    def show
-        @place = get_place()
-    end 
+  private
 
-    def edit
-        @place = get_place()
-    end
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
-    def update 
-        @place = get_place()
-       
-        if @place.update(places_params)
-          redirect_to @place
-        else
-          render 'edit'
-        end
-    end
-
-    private
-
-    def get_place
-        Place.find(params[:id])
-    end
-
-    def places_params
-        params.require(:place).permit(:postcode, :description)
-    end
+  def places_params
+    params.require(:place).permit(:postcode, :description)
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,58 +1,50 @@
 class RestaurantsController < ApplicationController
-    before_action :get_place
-    before_action :get_restaurant, only: %i[edit update destroy]
+  before_action :set_place
+  before_action :set_restaurant, only: %i[edit update destroy]
 
-    def index
-        @restaurants = @place.restaurants
+  def index
+    @restaurants = @place.restaurants
+  end
+
+  def new
+    @restaurant = @place.restaurants.new
+  end
+
+  def create
+    @restaurant = @place.restaurants.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = 'Restaurant has been created'
+      redirect_to place_restaurants_path(@place)
+    else
+      render :new
     end
+  end
 
-    def show
-
+  def update
+    if @restaurant.update(restaurant_params)
+    redirect_to place_restaurants_path(@place), notice: 'Restaurant has been edited'
+    else
+    render :edit
     end
+  end
 
-    def new
-        @restaurant = @place.restaurants.new()
-    end
+  def destroy
+    @restaurant.destroy
 
-    def create
-        @restaurant = @place.restaurants.new(restaurant_params)
-        if @restaurant.save
-          flash[:notice] = 'Restaurant has been created'
-          redirect_to place_restaurants_path(@place)
-        else
-          render :new
-        end
-    end
+    redirect_to place_restaurants_path(@place)
+  end
 
-    def edit
-    end
+  private
 
-    def update
-      if @restaurant.update(restaurant_params)
-        redirect_to place_restaurants_path(@place), notice: 'Restaurant has been edited'
-      else
-        render :edit
-      end
-    end
+  def set_place
+    @place = Place.find(params[:place_id])
+  end
 
-    def destroy
-      @place = get_place()
-      get_restaurant().destroy
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-      redirect_to @place
-    end
-
-    private
-
-    def get_place
-      @place = Place.find(params[:place_id])
-    end
-
-    def get_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
-
-    def restaurant_params
-        params.require(:restaurant).permit(:name, :address, :place_id)
-    end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :place_id)
+  end
 end
