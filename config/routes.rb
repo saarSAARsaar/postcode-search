e7a873e39cc77b056ef0f0cba2798593a3f2004e
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-11-12 13:55:38 +0100

    Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   resources :postcode_search, only: [:create]
   resources :places do
     resources :restaurants
   end
-
-  root 'places#index'
+  
+  root to: 'places#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
