c98ccb271882ba2bd4451b2945589a14c7fbf4e5
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-11-26 14:00:15 +0100

    Sign in with GitHub

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    places_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,13 @@ class User < ApplicationRecord
        :omniauthable, omniauth_providers: %i[github]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid, username: auth.info.nickname).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.email = auth.info.email
       user.uid = auth.uid
-      user.username = auth.info.nickame
+      user.username = auth.info.nickname
       user.token = auth.credentials.token
-      # user.username = auth.info.nickname
       user.password = Devise.friendly_token[0, 20]
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
     end
   end
 end
